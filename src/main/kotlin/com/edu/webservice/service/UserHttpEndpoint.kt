cb6f5e0683e78d4a6928194ab798b4f22576f750
san <sanantoha@gmail.com>
2019-02-14 00:16:16 +0300

    Add logback, delete started working unexpectedly.

--- src/main/kotlin/com/edu/webservice/service/UserHttpEndpoint.kt
+++ src/main/kotlin/com/edu/webservice/service/UserHttpEndpoint.kt
@@ -5,10 +5,8 @@ import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
-import java.time.LocalDate
 import java.time.LocalDateTime
 import java.util.*
-import javax.transaction.Transactional
 
 @RestController
 @RequestMapping("/users")
